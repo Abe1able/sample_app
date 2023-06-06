21c74b0bce4e3cd2a041c8de8c42d598bf86a166
Abel <abel.workmail@gmail.com>
2023-06-06 16:41:59 +0300

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/hello'
   resources :cars
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "pages#hello"
 end
